0cb3a172f7f8d3d54b33850dd54464fdf6dd5822
Ryan McCauley <ryanmccauley211@gmail.com>
2019-06-04 18:41:16 +0100

    Add contact form modals, styling and layout changes

--- static/js/event-listeners.js
+++ static/js/event-listeners.js
@@ -9,11 +9,10 @@ $('.main-menu-item a').each(function () {
 function transitionToPage(pageTarget) {
     if (pageTarget === "explore") {
 
-
         $('.svg-name-text').fadeOut(250, function () {
-            fadeBackgroundColorTo($(".page-half-partition-right"), 'white', 2000);
+            fadeBackgroundColorTo($(".page-half-partition-right"), 'white', 1000);
             fontShrink($(".main-menu-item a"), 0, 1000);
-            flexGrow($(".page-half-partition-left"), 0, 1000, function () {
+            flexGrow($(".page-half-partition-left"), 0, 750, function () {
                 window.location = "explore";
                 window.history.pushState(stateObj, 'Explore', '/explore');
             });
@@ -21,22 +20,26 @@ function transitionToPage(pageTarget) {
     }
 
     else if (pageTarget === "portfolio") {
-        fadeBackgroundColorTo($(".page-half-partition-right"), 'white', 2000);
-        fontShrink($(".main-menu-item a"), 0, 1000);
-        resizeSvg($('#name-svg'), 0, 0, 1000);
-        flexGrow($(".page-half-partition-left"), 0, 1000, function () {
-            window.location = "portfolio";
-            window.history.pushState(stateObj, 'Portfolio', '/portfolio');
+
+        $('.svg-name-text').fadeOut(250, function () {
+            fadeBackgroundColorTo($(".page-half-partition-right"), 'white', 1000);
+            fontShrink($(".main-menu-item a"), 0, 1000);
+            flexGrow($(".page-half-partition-left"), 0, 750, function () {
+                window.location = "portfolio";
+                window.history.pushState(stateObj, 'Portfolio', '/portfolio');
+            });
         });
     }
 
     else if (pageTarget === "contact") {
-        fadeBackgroundColorTo($(".page-half-partition-right"), 'white', 2000);
-        fontShrink($(".main-menu-item a"), 0, 1000);
-        resizeSvg($('#name-svg'), 0, 0, 1000);
-        flexGrow($(".page-half-partition-left"), 0, 1000, function () {
-            window.location = "contact";
-            window.history.pushState(stateObj, 'Contact', '/contact');
+
+        $('.svg-name-text').fadeOut(250, function () {
+            fadeBackgroundColorTo($(".page-half-partition-right"), 'white', 1000);
+            fontShrink($(".main-menu-item a"), 0, 1000);
+            flexGrow($(".page-half-partition-left"), 0, 750, function () {
+                window.location = "contact";
+                window.history.pushState(stateObj, 'Contact', '/contact');
+            });
         });
     }
 
