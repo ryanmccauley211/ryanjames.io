f322939b4b6d54c75a2c9fae598ca425612280e2
Ryan McCauley <ryanmccauley211@gmail.com>
2019-06-09 11:43:11 +0100

    Add smtp server and complete functionality of contact forms

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 	"html/template"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"os"
 )
 
+var email string
+var pass string
 var projectTodos = map[string]TodoList{}
 
 
@@ -40,10 +43,45 @@ func handleContact(w http.ResponseWriter, r *http.Request) {
 	if (err != nil) {
 		log.Fatalf("%v", err)
 	}
+
+	formSubmitted := false
+	if r.Method == "POST" {
+		handleSend(r)
+		formSubmitted = true
+	}
+
+	tmpl.Execute(w, formSubmitted)
+}
+
+func handleAbout(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("static/templates/about.html")
+	if (err != nil) {
+		log.Fatalf("%v", err)
+	}
 	tmpl.Execute(w, nil)
 }
 
+func handleSend(response *http.Request) {
+	response.ParseForm()
+	formType := response.PostFormValue("form-type")
+	if (formType == "hire") {
+		name := response.PostFormValue("first-name") + " " + response.PostFormValue("last-name") +
+			" contact: " + response.PostFormValue("contact-num");
+		SendMail("Hire", name, response.PostFormValue("email"), response.PostFormValue("message"), email, pass, response.PostFormValue("contact-num"), "")
+	} else if (formType == "contribute") {
+		name := response.PostFormValue("first-name") + " " + response.PostFormValue("last-name");
+		SendMail("Contribute to " + response.PostFormValue("project-name"), name,
+			response.PostFormValue("email"), response.PostFormValue("message"), email, pass, "", response.PostFormValue("project-name"))
+	} else if (formType == "other") {
+		name := response.PostFormValue("first-name") + " " + response.PostFormValue("last-name");
+		SendMail("General", name, response.PostFormValue("email"), response.PostFormValue("message"), email, pass, "", "")
+	}
+}
+
 func main() {
+
+	email = os.Args[1]
+	pass = os.Args[2]
 
 	fmt.Printf("Initializing...\n")
 	getTodoList("oak", "https://github.com/ryanmccauley211/Oak/blob/master/README.md")
@@ -53,9 +91,10 @@ func main() {
 	http.HandleFunc("/explore", handleExplore)
 	http.HandleFunc("/portfolio", handlePortfolio)
 	http.HandleFunc("/contact", handleContact)
+	http.HandleFunc("/about", handleAbout)
 
 	fmt.Println("Serving...\n")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":9000", nil))
 }
 
 func getTodoList(projectName string, url string) error {
